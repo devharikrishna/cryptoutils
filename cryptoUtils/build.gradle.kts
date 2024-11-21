900c3ac9088b4da73e646d517ece674ab4154264
TrogonNative <native.trogon@gmail.com>
2024-11-21 10:19:35 +0530

    check

--- cryptoUtils/build.gradle.kts
+++ cryptoUtils/build.gradle.kts
@@ -16,44 +16,12 @@ kotlin {
 }
 
 
-afterEvaluate {
-    publishing {
-        publications {
-            create<MavenPublication>("release") {
-                groupId = "com.github.devharikrishna"
-                artifactId = "cryptoutils"
-                version = "1.0.0"
-
-                //from(components["release"]) // Assuming "release" is your component name
-
-                // Configure POM metadata (optional but recommended)
-                /*pom {
-                    name.set("cryptoutils")
-                    description.set("Your library description")
-                    url.set("https://github.com/devharikrishna/cryptoutils") // Replace with your project URL
-                    licenses {
-                        license {
-                            name.set("The Apache License, Version 2.0")
-                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                        }
-                    }
-                    developers {
-                        developer {
-                            id.set("devharikrishna") // Replace with your developer ID
-                            name.set("Harikrishna K") // Replace with your name
-                            email.set("harikrishnaktkl619@gmail.com") // Replace with your email
-                        }
-                    }
-                    scm {
-                        connection.set("scm:git:github.com/devharikrishna/cryptoutils.git") // Replace with your Git URL
-                        developerConnection.set("scm:git:ssh://github.com/devharikrishna/cryptoutils.git") // Replace with your SSH Git URL
-                        url.set("https://github.com/devharikrishna/cryptoutils") // Replace with your project URL
-                    }
-                }*/
-
-
-
-            }
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            groupId = "com.github.devharikrishna"
+            artifactId = "cryptoutils"
+            version = "1.0.0"
         }
     }
 }
